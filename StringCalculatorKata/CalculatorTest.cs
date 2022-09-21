03feafcc509ee5e00e96d62a412ebd076803d1dd
Joakim Forslund <joakim.forslund@iths.se>
2022-09-21 20:13:02 +0200

    Test 2 *Done*

--- StringCalculatorKata/CalculatorTest.cs
+++ StringCalculatorKata/CalculatorTest.cs
@@ -20,6 +20,14 @@ namespace StringCalculatorKata
             Assert.Equal(expected, result);
         }
 
-        
+        [Theory]
+        [InlineData(10, "1,2,3,4")]
+        [InlineData(6, "2,2,2")]
+        public void ReturnNumberGivenSeveralCommaSeparatedNumbers(int expected, string input)
+        {
+            int result = Calculator.Add(input);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
